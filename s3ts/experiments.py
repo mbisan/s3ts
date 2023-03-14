859e19af7e5550477900f856a68affb2861e8648
Raúl Coterillo <rcoterillo@bcamath.org>
2023-03-14 18:05:03 +0100

    probemos

--- s3ts/experiments.py
+++ s3ts/experiments.py
@@ -192,7 +192,7 @@ def train_model(
                 n_labels=dm.n_labels, 
                 n_patterns=dm.n_patterns,
                 l_patterns=dm.l_patterns,
-                window_size=dm.window_size,
+                window_length=dm.window_length,
                 lab_shifts=[0],
                 arch=arch, 
                 learning_rate=learning_rate)
@@ -201,7 +201,7 @@ def train_model(
                 n_labels=dm.n_labels, 
                 n_patterns=1,
                 l_patterns=1,
-                window_size=dm.window_size,
+                window_length=dm.window_length,
                 lab_shifts=[0],
                 arch=arch, 
                 learning_rate=learning_rate)
@@ -482,7 +482,7 @@ def EXP_quant(
     
     results = pd.concat([base_results(dataset=dataset, fold_number=fold_number, arch=arch, pretrained=False, 
                         batch_size=batch_size, window_length=window_length, window_stride=window_stride, 
-                        random_state=random_state), 
+                        random_state=random_state),
                         data], axis = 1)
     results["nsamp_tra"] = len(train_dm.ds_train) + len(train_dm.ds_val)
     results["nsamp_pre"] = 0
@@ -508,13 +508,13 @@ def EXP_quant(
         # generate the dm with the correct number of quantiles
         _, pretrain_dm = prepare_dms(dataset=dataset,
             X_train=X_train, X_test=X_test, Y_train=Y_train, Y_test=Y_test,
-            batch_size=batch_size, window_size=window_length, window_stride=window_stride, 
+            batch_size=batch_size, window_length=window_length, window_stride=window_stride, 
             rho_dfs=rho_dfs, pret_frac=pret_frac, 
             quant_shifts=quant_shifts, quant_intervals=n_quant,
             nsamp_tra=nsamp_tra, nsamp_pre=nsamp_pre, nsamp_test=nsamp_test,
             fold_number=fold_number, random_state=random_state, 
             frames=arch.__frames__(), dir_cache=dir_cache)
-        train_dm: DoubleDataModule
+        pretrain_dm: DoubleDataModule
 
         results = base_results(dataset=dataset, fold_number=fold_number, arch=arch, pretrained=True, 
             batch_size=batch_size, window_length=window_length, window_stride=window_stride, 
@@ -609,61 +609,61 @@ def EXP_stride(
 
     runs = []
     STRIDES = [1,2,3,4,5]
-    trun, crun = 1+len(STRIDES), 0
-
-    # reset the seed
-    seed_everything(random_state)
-
-    crun += 1
-    log.info(f"~ [{crun}/{trun}] Training baseline model...")
-
-    # define the training directory        
-    date_flag = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    subdir_train = dir_train / f"EXP_{exp_name}_f{fold_number}.base_{date_flag}"
-
-    # run the base model
-    log.info("Training the complete model...")
-    data, model, checkpoint = train_model(
-        directory=subdir_train, label="target", 
-        epoch_max=tra_maxepoch,
-        dm=train_dm, arch=arch,
-        learning_rate=learning_rate)
-    
-    results = pd.concat([base_results(dataset=dataset, fold_number=fold_number, arch=arch, pretrained=False, 
-                        batch_size=batch_size, window_length=window_length, window_stride=window_stride, 
-                        random_state=random_state), 
-                        data], axis = 1)
-    results["nsamp_tra"] = len(train_dm.ds_train) + len(train_dm.ds_val)
-    results["nsamp_pre"] = 0
-    results["nsamp_test"] = len(train_dm.ds_test) 
-
-    # update results file
-    runs.append(results)
-    log.info(f"Updating results file ({str(res_file)})")
-    runs_df = pd.concat(runs, ignore_index=True)
-    runs_df.to_csv(res_file, index=False)
-
+    trun, crun = 2*len(STRIDES), 0
     
     for i, stride in enumerate(STRIDES):
-        
-        # set the pretrain data ratio
-        crun += 1 
-        log.info(f"~ [{crun}/{trun}] Checking with stride {stride}...")
-
-        # define the training directory   
-        date_flag = datetime.now().strftime("%Y-%m-%d_%H-%M")
-        subdir_train = dir_train / f"EXP_{exp_name}_f{fold_number}.{i}_{date_flag}"
 
         # generate the dm with the correct number of quantiles
-        _, pretrain_dm = prepare_dms(dataset=dataset,
+        train_dm, pretrain_dm = prepare_dms(dataset=dataset,
             X_train=X_train, X_test=X_test, Y_train=Y_train, Y_test=Y_test,
-            batch_size=batch_size, window_size=window_length, window_stride=stride, 
+            batch_size=batch_size, window_length=window_length, window_stride=stride, 
             rho_dfs=rho_dfs, pret_frac=pret_frac, 
             quant_shifts=quant_shifts, quant_intervals=quant_intervals,
             nsamp_tra=nsamp_tra, nsamp_pre=nsamp_pre, nsamp_test=nsamp_test,
             fold_number=fold_number, random_state=random_state, 
             frames=arch.__frames__(), dir_cache=dir_cache)
-        pretrain_dm: DoubleDataModule
+
+        # reset the seed
+        seed_everything(random_state)
+        
+        crun += 1
+        log.info(f"~ [{crun}/{trun}] Checking with stride {stride}...")
+
+        # define the training directory        
+        date_flag = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        subdir_train = dir_train / f"EXP_{exp_name}_f{fold_number}.{i}_base_{date_flag}"
+
+        # run the base model
+        log.info("Training target model...")
+        data, model, checkpoint = train_model(
+            directory=subdir_train, label="target", 
+            epoch_max=tra_maxepoch,
+            dm=train_dm, arch=arch,
+            learning_rate=learning_rate)
+        
+        results = pd.concat([base_results(dataset=dataset, fold_number=fold_number, arch=arch, pretrained=False, 
+                            batch_size=batch_size, window_length=window_length, window_stride=window_stride, 
+                            random_state=random_state), 
+                            data], axis = 1)
+        results["nsamp_tra"] = len(train_dm.ds_train) + len(train_dm.ds_val)
+        results["nsamp_pre"] = 0
+        results["nsamp_test"] = len(train_dm.ds_test) 
+
+        # update results file
+        runs.append(results)
+        log.info(f"Updating results file ({str(res_file)})")
+        runs_df = pd.concat(runs, ignore_index=True)
+        runs_df.to_csv(res_file, index=False)
+        
+        # set the pretrain data ratio
+        crun += 1 
+
+        # reset the seed
+        seed_everything(random_state)
+
+        # define the training directory   
+        date_flag = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        subdir_train = dir_train / f"EXP_{exp_name}_f{fold_number}.{i}_{date_flag}"
 
         results = base_results(dataset=dataset, fold_number=fold_number, arch=arch, pretrained=True, 
             batch_size=batch_size, window_length=window_length, window_stride=window_stride,
@@ -671,9 +671,6 @@ def EXP_stride(
         results["nsamp_tra"] = len(train_dm.ds_train) + len(train_dm.ds_val)
         results["nsamp_pre"] = len(pretrain_dm.ds_train) + len(pretrain_dm.ds_val)
         results["nsamp_test"] = len(train_dm.ds_test)
-
-        # reset the seed
-        seed_everything(random_state)
 
         # pretrain the encoder
         log.info("Training the encoder...")
@@ -686,7 +683,7 @@ def EXP_stride(
         encoder = model.encoder
 
         # train with the original task
-        log.info("Training the complete model...")
+        log.info("Training target model with pretrained encoder...")
         data, model, checkpoint = train_model(directory=subdir_train, label="target", 
             epoch_max=tra_maxepoch,
             dm=train_dm, arch=arch, encoder=encoder)
