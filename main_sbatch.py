8177c14becf4fb5b33b181558835a215f20a606a
Raúl Coterillo <rcoterillo@bcamath.org>
2023-05-04 18:28:17 +0200

    xd

--- main_sbatch.py
+++ main_sbatch.py
@@ -9,20 +9,22 @@ from s3ts.hooks import sbatch_hook
 
 # Common settings
 # ~~~~~~~~~~~~~~~~~~~~~~~
-PRETRAIN_DF = True                  # Pretrain DF
-TRAIN_DF = False                    # Train DF
-TRAIN_TS = False                    # Train TS
+PRETRAIN_ENCODERS = False         # Pretrain the DF encoders
+TIME_DIL = True                   # Time dilation
+PATT_STR = False                  # Pattern stride
+SELF_SUP = False                  # Self-supervised pretraining
 # ~~~~~~~~~~~~~~~~~~~~~~~
 DATASETS = [ # Datasets
     "CBF", #"GunPoint", "Plane", "SyntheticControl"                                           
 ]
 ARCHS = { # Architectures
-    "DF": ["CNN"],#, "ResNet"],
+    "DF": ["CNN", "ResNet"],
     "TS": ["RNN", "CNN", "ResNet"],
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~
+WINDOW_LENGTH_DF: int = 10                          # Window length for DF
 WINDOW_LENGTHS_TS: list[int] = [10, 30, 50, 70]     # Window length for TS                   
-WINDOW_LENGTHS_DF: list[int] = [10]                 # Window length for DF
+
 WINDOW_TIME_STRIDES: list[int] = [1, 3, 5, 7]       # Window time stride
 WINDOW_PATT_STRIDES: list[int] = [2, 3, 5]          # Window pattern stride
 # ~~~~~~~~~~~~~~~~~~~~~~~
@@ -62,42 +64,44 @@ SHARED_ARGS = {**SHARED_ARGS, **HIPATIA_MEDIUM}
 # Pretrain Loop
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-if PRETRAIN_DF:
+if PRETRAIN_ENCODERS:
     for arch in ARCHS["DF"]:
         for dataset in DATASETS:
             res_fname = f"results_pretrain_{arch}_{dataset}.csv"
-            for wlen in WINDOW_LENGTHS_DF:
-                for wts in WINDOW_TIME_STRIDES:
-                    # Full series
+            wlen = WINDOW_LENGTH_DF
+            for wts in WINDOW_TIME_STRIDES:
+                # Full series
+                sbatch_hook(dataset=dataset, mode="DF", arch=arch,
+                    use_pretrain=False, pretrain_mode=True,
+                    window_length=wlen, stride_series=False,
+                    window_time_stride=wts, window_patt_stride=1,
+                    max_epochs=MAX_EPOCHS_PRE, cv_rep=0, 
+                    res_fname=res_fname, **SHARED_ARGS)
+                if wts != 1:
+                    # Strided series
                     sbatch_hook(dataset=dataset, mode="DF", arch=arch,
                         use_pretrain=False, pretrain_mode=True,
-                        window_length=wlen, stride_series=False,
+                        window_length=wlen, stride_series=True,
                         window_time_stride=wts, window_patt_stride=1,
                         max_epochs=MAX_EPOCHS_PRE, cv_rep=0, 
                         res_fname=res_fname, **SHARED_ARGS)
-                    if wts != 1:
-                        # Strided series
-                        sbatch_hook(dataset=dataset, mode="DF", arch=arch,
-                            use_pretrain=False, pretrain_mode=True,
-                            window_length=wlen, stride_series=True,
-                            window_time_stride=wts, window_patt_stride=1,
-                            max_epochs=MAX_EPOCHS_PRE, cv_rep=0, 
-                            res_fname=res_fname, **SHARED_ARGS)
-                for wps in WINDOW_PATT_STRIDES:
-                    # Full series
-                    sbatch_hook(dataset=dataset, mode="DF", arch=arch,
-                        use_pretrain=False, pretrain_mode=True,
-                        window_length=wlen, stride_series=False,
-                        window_time_stride=7, window_patt_stride=wps,
-                        max_epochs=MAX_EPOCHS_PRE, cv_rep=0, 
-                        res_fname=res_fname, **SHARED_ARGS)
+            for wps in WINDOW_PATT_STRIDES:
+                # Full series
+                sbatch_hook(dataset=dataset, mode="DF", arch=arch,
+                    use_pretrain=False, pretrain_mode=True,
+                    window_length=wlen, stride_series=False,
+                    window_time_stride=7, window_patt_stride=wps,
+                    max_epochs=MAX_EPOCHS_PRE, cv_rep=0, 
+                    res_fname=res_fname, **SHARED_ARGS)
 
-# Training Loop for TS
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Training Loop for Section 5.1: Time Dilation
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-if TRAIN_TS:
-    mode = "TS"
+if TIME_DIL:
     for cv_rep in range(CV_REPS):
+
+        # Do the TS training
+        mode = "TS"
         for arch in ARCHS[mode]:
             for dataset in DATASETS:
                 res_fname = f"results_{mode}_{arch}_{dataset}_cv{cv_rep}.csv"
@@ -108,42 +112,83 @@ if TRAIN_TS:
                         window_time_stride=1, window_patt_stride=1,
                         max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
                         res_fname=res_fname, **SHARED_ARGS)
-
-
-# Training Loop for DF 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-if TRAIN_DF:
-    mode = "DF"
-    for cv_rep in range(CV_REPS):
+                    
+        # Do the DF training
+        mode = "DF"
         for arch in ARCHS[mode]:
             for dataset in DATASETS:
+
                 res_fname = f"results_{mode}_{arch}_{dataset}_cv{cv_rep}.csv"
-                for wlen in WINDOW_LENGTHS_DF:
-                    for wts in WINDOW_TIME_STRIDES:
-                        # Full series
-                        for use_pretrain in [False, True]:
-                            sbatch_hook(dataset=dataset, mode=mode, arch=arch,
-                                use_pretrain=use_pretrain, pretrain_mode=False,
-                                window_length=wlen, stride_series=False,
-                                window_time_stride=wts, window_patt_stride=1,
-                                max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
-                                res_fname=res_fname, **SHARED_ARGS)
-                        if wts != 1:
-                            # Strided series
-                            for use_pretrain in [False, True]:
-                                sbatch_hook(dataset=dataset, mode=mode, arch=arch,
-                                    use_pretrain=use_pretrain, pretrain_mode=False,
-                                    window_length=wlen, stride_series=True,
-                                    window_time_stride=wts, window_patt_stride=1,
-                                    max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
-                                    res_fname=res_fname, **SHARED_ARGS)
-                    for wps in WINDOW_PATT_STRIDES:
-                        # Full series
-                        for use_pretrain in [False, True]:
-                            sbatch_hook(dataset=dataset, mode=mode, arch=arch,
-                                use_pretrain=use_pretrain, pretrain_mode=False,
-                                window_length=wlen, stride_series=False,
-                                window_time_stride=7, window_patt_stride=wps, 
-                                max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
-                                res_fname=res_fname, **SHARED_ARGS)
+
+                wlen = WINDOW_LENGTH_DF
+                wps  = 1
+
+                for wts in WINDOW_TIME_STRIDES:
+                    sbatch_hook(dataset=dataset, mode=mode, arch=arch,
+                        use_pretrain=False, pretrain_mode=False,
+                        window_length=wlen, stride_series=False,
+                        window_time_stride=wts, window_patt_stride=wps,
+                        max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
+                        res_fname=res_fname, **SHARED_ARGS)
+
+# Training Loop for Section 5.2: Pattern Stride
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if PATT_STR:
+    for cv_rep in range(CV_REPS):
+                    
+        # Do the DF training
+        mode = "DF"
+        for arch in ARCHS[mode]:
+            for dataset in DATASETS:
+
+                res_fname = f"results_{mode}_{arch}_{dataset}_cv{cv_rep}.csv"
+
+                wlen = WINDOW_LENGTH_DF
+                wts  = 7
+
+                for wps in WINDOW_PATT_STRIDES:
+                    # Full series
+                    sbatch_hook(dataset=dataset, mode=mode, arch=arch,
+                        use_pretrain=False, pretrain_mode=False,
+                        window_length=wlen, stride_series=False,
+                        window_time_stride=wts, window_patt_stride=wps, 
+                        max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
+                        res_fname=res_fname, **SHARED_ARGS)
+
+
+# Training Loop for Section 5.3: Self-Supervised Pretraining
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#TODO
+if SELF_SUP:
+    for cv_rep in range(CV_REPS):
+                    
+        # Do the DF training
+        mode = "DF"
+        for arch in ARCHS[mode]:
+            for dataset in DATASETS:
+
+                res_fname = f"results_{mode}_{arch}_{dataset}_cv{cv_rep}.csv"
+                
+                wlen = WINDOW_LENGTH_DF
+                wts  = 7
+                wps  = 2 
+
+                # Full series
+                sbatch_hook(dataset=dataset, mode=mode, arch=arch,
+                    use_pretrain=True, pretrain_mode=False,
+                    window_length=wlen, stride_series=False,
+                    window_time_stride=wts, window_patt_stride=wps, 
+                    max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
+                    res_fname=res_fname, **SHARED_ARGS)
+                
+                # Strided series
+                sbatch_hook(dataset=dataset, mode=mode, arch=arch,
+                    use_pretrain=True, pretrain_mode=False,
+                    window_length=wlen, stride_series=False,
+                    window_time_stride=wts, window_patt_stride=wps, 
+                    max_epochs=MAX_EPOCHS_TRA, cv_rep=cv_rep, 
+                    res_fname=res_fname, **SHARED_ARGS)
+
+
