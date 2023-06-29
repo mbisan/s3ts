36c8c6009d45fa97c2d5d5919f9ef38bc6f687b8
Raúl Coterillo <raulcote98@gmail.com>
2023-06-29 07:01:44 +0200

    graphs

--- model_sizes.py
+++ model_sizes.py
@@ -1,20 +1,30 @@
 
 from pytorch_lightning.utilities.model_summary import ModelSummary
 from s3ts.models.wrapper import WrapperModel
+from torchview import draw_graph
 
-model = WrapperModel(mode="df",
-             arch="dfn",
+draw = 0
+npat = 2
+wlen = 10
+lpat = 100
+
+model = WrapperModel(mode="ts",
+             arch="res",
              target="cls", # "cls"
-             n_classes=2,
-             n_patterns=2,
-             l_patterns=100,
-             window_length=70,
+             n_classes=npat,
+             n_patterns=npat,
+             l_patterns=lpat,
+             window_length=wlen,
              stride_series=False,
              window_time_stride=1,
              window_patt_stride=1,
-             encoder_feats=20,
+             encoder_feats=16,
              decoder_feats=64,
              learning_rate=1E-4)
              
 summary = ModelSummary(model, max_depth=2)
 print(summary)
+
+if draw:
+    model_graph = draw_graph(model, input_size=(1, 1, wlen), expand_nested=True)
+    model_graph.visual_graph.render('model_graph', view=True)
