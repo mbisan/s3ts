190abba04fbc79a34d19b3e9200d0b69aa7a838e
Raúl Coterillo <raulcote98@gmail.com>
2023-07-10 15:17:50 +0200

    xd

--- s3ts/cli.py
+++ s3ts/cli.py
@@ -247,8 +247,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''Perform the experiments showcased in the article.''')
 
     parser.add_argument('--dataset', type=str, required=True,
-        choices = ["GunPoint", "Plane", "CBF", "Fish", "Trace", "Chinatown", "OSULeaf", 
-                    "PowerCons", "Car", "ECG200", "ArrowHead"],
+        choices = ["ArrowHead", "CBF", "ECG200", "GunPoint", "SyntheticControl", "Trace"],
         help='Name of the dataset from which create the DTWs')
 
     parser.add_argument('--mode', type=str, required=True, choices=['ts', 'df', 'gf'],
