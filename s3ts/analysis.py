9ad4d28bbb42cf99a31b658cae3e2cccca76c803
Raúl Coterillo <raulcote98@gmail.com>
2023-09-19 10:09:20 +0200

    horizontal figures

--- s3ts/analysis.py
+++ s3ts/analysis.py
@@ -80,12 +80,12 @@ def results_table(df: pd.DataFrame,
     return table
 
 
-def timedil_figure(
+def timedil_figure_vertical(
         df: pd.DataFrame,
         fontsize: int = 18,
         metric: str = "test_acc",
         metric_lab: str = "Test Accuracy",
-        fname: str = "figures/ablation/timedil.pdf",
+        fname: str = "figures/ablation/timedilv.pdf",
         ) -> pd.DataFrame:
     
     """ Generates the time dilation figure for the paper. """
@@ -153,14 +153,84 @@ def timedil_figure(
             if col_val == "DF":
                 ax.text(1, -0.32, "Context Size", ha="center", va="center", transform=ax.transAxes, fontsize=fontsize-2)
 
-
     g.savefig(fname, bbox_inches='tight')
 
-def pretrain_figure(df: pd.DataFrame,
+
+def timedil_figure_horizontal(
+        df: pd.DataFrame,
         fontsize: int = 18,
         metric: str = "test_acc",
         metric_lab: str = "Test Accuracy",
-        fname: str = "figures/ablation/pretrain.pdf"
+        fname: str = "figures/ablation/timedilh.pdf",
+        ) -> pd.DataFrame:
+    
+    """ Generates the time dilation figure for the paper. """
+
+    # Filter the data
+    data = df[df["pretrain_mode"] == False].copy()
+    data = data[data['train_exc_limit'] == 32]
+    data = data[data["window_patt_stride"] == 1]
+    data = data[data["pretrained"] == False]
+    data = data[~data["mode"].isin(["ts"])]
+    
+    # Convert metric to percentage
+    data[metric] = data[metric]*100
+
+    # Generate a plot
+    data["Method"] =  data["arch"] + "_" + data["mode"]
+    data.sort_values(["Method"], inplace=True)
+
+    data["arch"].replace(to_replace=["rnn", "cnn", "res"], value=["RNN", "CNN", "RES"], inplace=True)
+    data["mode"].replace(to_replace=["df", "gf"], value=["DF", "GAF"], inplace=True)
+    data["Arch"] = data["arch"]
+
+    sns.set_theme()
+    sns.set_style("whitegrid")
+    plt.rc('font', family='serif', serif='Times New Roman', size=fontsize)
+    plt.rc('xtick', labelsize='medium')
+    plt.rc('ytick', labelsize='medium')
+
+    g: sns.FacetGrid = sns.relplot(data=data, x="eq_wdw_length", y=metric, hue='Arch', 
+        kind="line", col="dataset", row="mode", palette=sns.color_palette("bright"),
+        height=2.5, aspect=1, legend="auto", markers="True", row_order=["DF", "GAF"], 
+        col_order=["ArrowHead", "CBF", "ECG200", "GunPoint", "SyntheticControl", "Trace"],
+        facet_kws={"despine": False, "margin_titles": True, "legend_out": True,
+                   "sharey": False, "sharex": True})
+    g.set_titles(col_template="{col_name}", row_template="{row_name}", size=fontsize)
+    g.set_xlabels("Equivalent Window Length", fontsize=fontsize-2);
+    g.set_ylabels(metric_lab, fontsize=fontsize-2);
+    g.set(xlim=(5, 75), xticks=[10, 30, 50, 70])
+    g.figure.subplots_adjust(wspace=0, hspace=0)
+    g.legend.set_title("")
+
+    dsets = ["ArrowHead", "CBF", "ECG200", "GunPoint", "SyntheticControl", "Trace"]
+    ybound = ([35, 75], [40, 50, 60, 70])
+
+    for i, row in enumerate(g.axes):
+        for j, ax in enumerate(row):
+            ax.set_ylim(ybound[0])
+            ax.set_yticks(ybound[1])
+
+    for (row_val, col_val), ax in g.axes_dict.items():
+        if col_val not in ["ArrowHead"]:
+            ax.set_yticklabels([])
+        if col_val not in ["CBF", "SyntheticControl"]:
+            ax.set_ylabel("")
+            ax.set_xlabel("")
+        for sp in ax.spines.values():
+            sp.set_color("black")
+        if row_val == "Trace":
+            ax.set_xlabel("")
+            if col_val == "DF":
+                ax.text(1, -0.32, "Context Size", ha="center", va="center", transform=ax.transAxes, fontsize=fontsize-2)
+
+    g.savefig(fname, bbox_inches='tight')
+
+def pretrain_figure_vertical(df: pd.DataFrame,
+        fontsize: int = 18,
+        metric: str = "test_acc",
+        metric_lab: str = "Test Accuracy",
+        fname: str = "figures/ablation/pretrainv.pdf"
         ) -> pd.DataFrame:
 
     data = df[df["pretrain_mode"] == False].copy()
@@ -218,6 +288,74 @@ def pretrain_figure(df: pd.DataFrame,
             ax.set_yticklabels([])
         if row_val not in ["CBF", "SyntheticControl"]:
             ax.set_ylabel("")
+        for sp in ax.spines.values():
+            sp.set_color("black")
+        if row_val == "Trace":
+            ax.set_xlabel("")
+            if col_val == "DF":
+                ax.text(1, -0.32, "Labeled events per class", ha="center", va="center", transform=ax.transAxes, fontsize=fontsize-2)
+
+    g.savefig(fname, bbox_inches='tight')
+
+
+def pretrain_figure_horizontal(df: pd.DataFrame,
+        fontsize: int = 18,
+        metric: str = "test_acc",
+        metric_lab: str = "Test Accuracy",
+        fname: str = "figures/ablation/pretrainh.pdf"
+        ) -> pd.DataFrame:
+
+    data = df[df["pretrain_mode"] == False].copy()
+
+    data = data[data["window_time_stride"] == 7]
+    data = data[data["window_patt_stride"] == 1]
+
+    data = data[data["arch"].isin(["cnn", "res"])]
+
+    # Convert metric to percentage
+    data[metric] = data[metric]*100
+
+    data["arch"].replace(to_replace=["cnn", "res"], value=["CNN", "RES"], inplace=True)
+    data["mode"].replace(to_replace=["df", "gf"], value=["DF", "GAF"], inplace=True)
+    data["Arch"] = data["arch"] + data["pretrained"].replace({True: "-", False: ""}) + data["stride_series"].replace({True: "B", False: "A"})
+    data["Arch"].replace({"CNNA": "CNN", "RESA": "RES"}, inplace=True)
+
+    sns.set_theme()
+    sns.set_style("whitegrid")
+    plt.rc('font', family='serif', serif='Times New Roman', size=fontsize)
+    plt.rc('xtick', labelsize='medium')
+    plt.rc('ytick', labelsize='medium')
+
+    g: sns.FacetGrid = sns.relplot(data=data, x="train_exc_limit", y=metric, hue='Arch', 
+        kind="line", col="dataset", row="mode", palette=sns.color_palette("bright"),
+        height=2.5, aspect=1, legend="brief", markers="True", row_order=["DF", "GAF"], 
+        col_order=["ArrowHead", "CBF", "ECG200", "GunPoint", "SyntheticControl", "Trace"],
+        facet_kws={"despine": False, "margin_titles": True, "legend_out": True,
+                   "sharey": False, "sharex": True})
+
+    g.set_titles(col_template="{col_name}", row_template="{row_name}", size=fontsize)
+    g.set_xlabels("Labeled events per class", fontsize=fontsize-2);
+    g.set_ylabels(metric_lab, fontsize=fontsize-2);
+    g.set(xlim=(2, 34), xticks=[8,16,24,32])
+    #    ylim=[15, 80], yticks=[20, 30, 40, 50, 60, 70])
+    g.figure.subplots_adjust(wspace=0, hspace=0)
+    g.legend.__setattr__("fontsize", fontsize)
+    g.legend.set_title("")
+
+
+    dsets = ["ArrowHead", "CBF", "ECG200", "GunPoint", "SyntheticControl", "Trace"]
+    ybound = ([25, 85], [30, 40, 50, 60, 70, 80])
+
+    for i, row in enumerate(g.axes):
+        for j, ax in enumerate(row):
+            ax.set_ylim(ybound[0])
+            ax.set_yticks(ybound[1])
+    for (row_val, col_val), ax in g.axes_dict.items():
+        if col_val not in ["ArrowHead"]:
+            ax.set_yticklabels([])
+        if col_val not in ["CBF", "SyntheticControl"]:
+            ax.set_ylabel("")
+            ax.set_xlabel("")
         for sp in ax.spines.values():
             sp.set_color("black")
         if row_val == "Trace":
@@ -351,10 +489,12 @@ if __name__ == "__main__":
     #print(results_table(df))
 
     # timedil figure
-    timedil_figure(df)
+    # timedil_figure_vertical(df)
+    # timedil_figure_horizontal(df)
 
     # pretrain figure
     #pretrain_figure(df)
+    pretrain_figure_horizontal(df)
 
     # encoding plots
     # for dset in ["ArrowHead", "CBF", "ECG200", "GunPoint", "SyntheticControl", "Trace", "TwoLeadECG"]:
