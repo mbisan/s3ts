b37e6d9e188fe0b60628175d59a350b499552a31
mbisan <mbikandi@bcamath.org>
2023-11-20 10:52:49 +0100

    fix + staristics on HARTH

--- storage/prepare_har_dataset.py
+++ storage/prepare_har_dataset.py
@@ -3,9 +3,14 @@ import numpy as np
 import os
 import datetime
 
+from label_mappings import *
+
 def prepare_harth(dataset_dir):
 
     ds = []
+
+    counts = {}
+    event_length = {}
 
     for dir, _, files in os.walk(os.path.join(dataset_dir, "harth")):
         for i, file in enumerate(files):
@@ -21,38 +26,67 @@ def prepare_harth(dataset_dir):
                             datetime.datetime.strptime(ds["timestamp"][0], "%Y-%m-%d %H:%M:%S.%f")) / datetime.timedelta(milliseconds=1), ds["timestamp"]) )
 
             # check if the sampling rate is correct, at 50hz
-            # all .csv files have some timestamps with jumps of less or more than 20ms
-            # for jumps with less than 10ms, we remove observations, for jumps with more than 20ms, we consider a new STS
+            # all .csv files have some timestamps with jumps of less or more than 15ms
+            # for jumps with less than 10ms, we remove observations, for jumps with more than 25ms, we consider a new STS
 
-            # remove observations closer than 20ms from each other
             remove = []
             j = 0
             for i in range(len(ds) - 1):
-                if (ds["dt"][i + 1] - ds["dt"][j]) < 20:
+                diff = ds["dt"][i + 1] - ds["dt"][j]
+                if diff < 15:
                     remove.append(i + 1)
                 else:
-                    j = i
+                    j = i + 1
 
+            print(f"Removed {len(remove)} observations.")
             ds.drop(remove, inplace=True)
+            ds = ds.reset_index()
 
-            # extract STS from the data (segments with all the points in between at most 20ms apart)
             splits = []
             last = 0
             for i in range(len(ds) - 1):
-                if (ds["dt"][i + 1] - ds["dt"][i]) > 20:
+                if (ds["dt"][i + 1] - ds["dt"][i]) > 25:
                     splits.append(ds.loc[last:i])
                     last = i + 1
+            splits.append(ds.loc[last:len(ds)])
 
-            # print(np.count_nonzero(remove))
-            if not os.path.exists(os.path.join(dataset_dir, f"{file.replace('.', '_')}")):
-                os.mkdir(os.path.join(dataset_dir, f"{file.replace('.', '_')}"))
+            if not os.path.exists(os.path.join(dataset_dir, f"{file.replace('.csv', '')}")):
+                os.mkdir(os.path.join(dataset_dir, f"{file.replace('.csv', '')}"))
 
             for i, sp in enumerate(splits):
-                with open(os.path.join(dataset_dir, f"{file.replace('.', '_')}/acc{i}.npy"), "wb") as f:
+                labels = sp[["label"]].to_numpy()
+
+                with open(os.path.join(dataset_dir, f"{file.replace('.csv', '')}/acc{i}.npy"), "wb") as f:
                     np.save(f, sp[["back_x", "back_y", "back_z", "thigh_x", "thigh_y", "thigh_z"]].to_numpy())
                 
-                with open(os.path.join(dataset_dir, f"{file.replace('.', '_')}/label{i}.npy"), "wb") as f:
-                    np.save(f, sp[["label"]].to_numpy())
+                with open(os.path.join(dataset_dir, f"{file.replace('.csv', '')}/label{i}.npy"), "wb") as f:
+                    np.save(f, labels)
+                
+                # update class counts
+                lb, c = np.unique(labels, return_counts=True)
+                for i, l in enumerate(lb):
+                    counts[l] = counts.get(l, 0) + c[i]
+
+                # update event counts and event length
+                current_event = 0
+                for i in range(1, labels.size - 1):
+                    if labels[i] != labels[current_event]:
+                        event_length[int(labels[current_event])] = \
+                            event_length.get(int(labels[current_event]), []) + [i - current_event]
+                        current_event = i
+                
+                # last event
+                event_length[int(labels[current_event])] = \
+                            event_length.get(int(labels[current_event]), []) + [labels.size - current_event]
+    
+    # print statistics
+    total = sum(counts.values())
+    print(f"Total number of observations: {total}")
+
+    for c in counts.keys():
+            print(f"{len(event_length[c])} events in class {HARTH_LABELS[c]},")
+            print(f"\twith size (min) {min(event_length[c])}, (max) {max(event_length[c])}, (mean) {np.mean(event_length[c])}")
+            print(f"\t{counts[c]} observations ({(counts[c]/total):.2f})")
 
 if __name__ == "__main__":
     prepare_harth("storage/datasets/HARTH")
