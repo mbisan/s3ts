c8390425b2b800d730e23186c5e33696c84bdac9
mbisan <mbikandi@bcamath.org>
2023-11-30 14:34:44 +0100

    dtw_layer_tests added

--- storage/har_datasets.py
+++ storage/har_datasets.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import torch
 
 from torch.utils.data import Dataset
 
@@ -117,6 +118,60 @@ def sts_barycenter(dataset: STSDataset, n: int = 100, random_seed: int = 45):
         selected[i] = km.cluster_centers_[0].T
 
     return selected
+
+class StreamingTimeSeries(STSDataset):
+
+    def __init__(self,
+            STS: np.ndarray,
+            SCS: np.ndarray,
+            wsize: int = 10,
+            wstride: int = 1,
+            normalize: bool = True,
+            label_mapping: np.ndarray = None
+            ) -> None:
+        super().__init__(wsize=wsize, wstride=wstride)
+
+        self.STS = STS
+        self.SCS = SCS
+
+        self.splits = np.array([0, SCS.shape[0]])
+
+        # process ds
+        self.label_mapping = label_mapping
+        if not self.label_mapping is None:
+            self.SCS = self.label_mapping[self.SCS]
+
+        self.indices = np.arange(self.SCS.shape[0])
+        for i in range(wsize * wstride):
+            self.indices[self.splits[:-1] + i] = 0
+        self.indices = self.indices[np.nonzero(self.indices)]
+
+        if normalize:
+            self.normalizeSTS("normal")
+
+
+class StreamingTimeSeriesCopy(Dataset):
+
+    def __init__(self,
+            stsds: StreamingTimeSeries, indices: np.ndarray
+            ) -> None:
+        super().__init__()
+
+        self.stsds = stsds
+        self.indices = indices
+        
+    def __len__(self):
+        return self.indices.shape[0]
+    
+    def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor, int]:
+
+        ts, c = self.stsds[self.indices[index]]
+        return {"series": ts, "label": c}
+    
+    def __del__(self):
+        del self.stsds
+
+# Load datasets predefined
 
 class UCI_HARDataset(STSDataset):
 
