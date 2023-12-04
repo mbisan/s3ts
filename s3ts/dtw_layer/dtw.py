10aeda441cedb036f84654734d57f7c99cd1ca39
mbisan <mbikandi@bcamath.org>
2023-12-04 15:52:15 +0100

    dtw_faster v4

--- s3ts/dtw_layer/dtw.py
+++ s3ts/dtw_layer/dtw.py
@@ -2,19 +2,6 @@ import torch
 
 @torch.jit.script
 def dtw_compute(dist_tensor: torch.Tensor, grad_tensor: torch.Tensor, w: float) -> None:
-    for i in range(1, dist_tensor.shape[2]):
-        for j in range(1, dist_tensor.shape[3]):
-            # elements has shape (3, n, k)
-            elements = torch.stack([w * dist_tensor[:, :, i, j-1], dist_tensor[:, :, i-1, j], w * dist_tensor[:, :, i-1, j-1]], dim=0)
-
-            value, id = torch.min(elements, dim=0) # shape (n, k)
-
-            dist_tensor[:,:, i, j] += value
-
-            grad_tensor[id==0][:, :, i, j] += w * grad_tensor[id==0][:, :, i, j-1]
-
-@torch.jit.script
-def dtw_compute_faster(dist_tensor: torch.Tensor, grad_tensor: torch.Tensor, w: float) -> None:
     for i in range(1, dist_tensor.shape[0]):
         for j in range(1, dist_tensor.shape[1]):
             value = torch.minimum(w * torch.minimum(dist_tensor[i, j-1], dist_tensor[i-1, j-1]), dist_tensor[i-1, j])
@@ -25,50 +12,7 @@ def dtw_compute_faster(dist_tensor: torch.Tensor, grad_tensor: torch.Tensor, w: 
             grad_tensor[id][:, :, i, j] += w * grad_tensor[id][:, :, i, j-1]
 
 @torch.jit.script
-def dtw_compute_by_index(dist_tensor: torch.Tensor, grad_tensor: torch.Tensor, w: float, n: int, s: int) -> None:
-    for i in range(1, dist_tensor.shape[2]):
-        for j in range(1, dist_tensor.shape[3]):
-            elements = torch.stack([w * dist_tensor[n, s, i, j-1], dist_tensor[n, s, i-1, j], w * dist_tensor[n, s, i-1, j-1]], dim=0)
-
-            value, id = torch.min(elements, dim=0)
-
-            dist_tensor[n, s, i, j] += value
-
-            grad_tensor[id==0][n, s, i, j] += w * grad_tensor[id==0][n, s, i, j-1]
-
-@torch.jit.script
 def torch_dtw_fast(x: torch.Tensor, y: torch.Tensor, w: float, eps: float = 1e-5):
-    # shape of x (n, dim, x_len) y (m, dim, y_len)    
-    # performs convolution-like operation, for each kernel the DF
-    # (of shape (kernel_size, T)) is computed, then summed across channels
-    # x has shape (batch, c, time_dimension)
-
-    # compute pairwise diffs (squared)
-    p_diff = x[:,None,:,None,:] - y[None,:,:,:,None] # shape (n, n_kernel, d, Kernel_size, T)
-    euc_d = torch.square(p_diff).sum(2) # shape (n, n_kernel, kernel_size, T)
-
-    # compute dtw
-    DTW = euc_d.clone()
-    DTW[:,:,0,:] = torch.cumsum(DTW[:,:,0,:], dim=2)
-    DTW[:,:,:,0] = torch.cumsum(DTW[:,:,:,0], dim=2)
-
-    # p_diff contains the partial derivatives of DTW[n, k, i, j] wrt K[k, d, i] (dims (n, k, d, i, j))
-    p_diff = p_diff / torch.sqrt(euc_d[:,:, None, :, :] + eps)
-
-
-    dtw_compute(DTW, p_diff, w)
-    # futures : List[torch.jit.Future[None]] = []
-    # for n in range(DTW.shape[0]):
-    #     for s in range(DTW.shape[1]):
-    #         futures.append(torch.jit.fork(dtw_compute_by_index, DTW, p_diff, w, n, s))
-
-    # for future in futures:
-    #     torch.jit.wait(future)
-
-    return DTW.sqrt(), p_diff
-
-@torch.jit.script
-def torch_dtw_faster(x: torch.Tensor, y: torch.Tensor, w: float, eps: float = 1e-5):
     # shape of x (n, dim, x_len) y (m, dim, y_len)
 
     # performs convolution-like operation, for each kernel the DF
@@ -89,7 +33,7 @@ def torch_dtw_faster(x: torch.Tensor, y: torch.Tensor, w: float, eps: float = 1e
     # rearrange dims
     DTW = torch.permute(euc_d, (2, 3, 0, 1)).contiguous()
 
-    dtw_compute_faster(DTW, p_diff, w)
+    dtw_compute(DTW, p_diff, w)
 
     # recover dimensions
     DTW = torch.permute(DTW, (2, 3, 0, 1)).contiguous()
@@ -100,7 +44,7 @@ class torch_dtw(torch.autograd.Function):
 
     @staticmethod
     def forward(x, y, w):
-        DTW, p_diff = torch_dtw_faster(x, y, w)
+        DTW, p_diff = torch_dtw_fast(x, y, w)
         return DTW, p_diff
     
     @staticmethod
